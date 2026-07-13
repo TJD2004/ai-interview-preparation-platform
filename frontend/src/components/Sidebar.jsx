7fa974411d0582e4a3081ae1ad8086060b75daba
TJD2004 <dharmiktushar2004@gmail.com>
2026-07-13 21:20:44 +0530

    project completed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -26,7 +26,7 @@ export default function Sidebar() {
       transition={{ duration: 0.4 }}
     >
       <div className="sidebar-brand-row">
-        <Logo size={40} />
+        <Logo size={30} />
         <span className="sidebar-brand">InterviewPrep</span>
       </div>
 
